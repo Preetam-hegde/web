bf783ead7786a3e35e3009a39ec793260e96bd28
Preetam <preetamhegde.is19@bmsce.ac.in>
2024-01-11 16:40:52 +0530

    sweet money

---
+++ financeTracker/script.js
@@ -0,0 +1,54 @@
+function getTodayDate() {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const day = String(today.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
+
+document.getElementById('datePicker').value = getTodayDate();
+
+const form = document.getElementById('waste-form');
+
+async function sendData() {
+  
+    const formData ={};
+    formData['expense_name']=document.getElementById("expense_name").value;
+    formData['expense_rs']=document.getElementById("expense_rs").value;
+    formData['category']=document.getElementById("cat").value;
+    formData['date']=document.getElementById("datePicker").value;
+
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer h1fSj_TDMQ-lVN_WUn3RKPSubpz0ENNQoWWyBthYqryh2Qhj5xR7N28kZdA");
+    myHeaders.append("X-Spreadsheet-Id", "1LArqac_OgzHg7E-FHep8GjsxLy0PWlPKbRS3dM5N2T8");
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Cookie", "_cfuvid=9FeoLb_oWmPRe_eH9MedE0zO9EGKHhRjdHDNED_rMrU-1704957294765-0-604800000");
+
+    var raw = JSON.stringify(formData);
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
+ 
+    
+
+    fetch("https://api.sheetson.com/v2/sheets/Main", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+    
+    
+}
+
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  sendData();
+  form.reset();
+  document.getElementById('datePicker').value = getTodayDate();
+
+});
